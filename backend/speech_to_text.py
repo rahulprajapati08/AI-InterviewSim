7651d53e41adcc81fa390321f2a6ea7c541efc48
Rahul Prajapati <132542774+rahulprajapati08@users.noreply.github.com>
2025-07-22 20:57:40 +0530

    Update speech_to_text.py

--- backend/speech_to_text.py
+++ backend/speech_to_text.py
@@ -1,32 +1,8 @@
-import sounddevice as sd 
-from scipy.io.wavfile import write
+# speech_to_text.py
 import whisper
-import numpy as np
-import keyboard
-import tempfile
 
 model = whisper.load_model('base')
 
-def record_audio(fs=44100, key='enter'):
-  print("🎙️ Recording... (press Enter to stop)")
-  audio_buffer = []
-
-  def callback(indata, frames, time, status):
-      audio_buffer.append(indata.copy())
-
-  with sd.InputStream(samplerate=fs, channels=1, callback=callback):
-      while True:
-          if keyboard.is_pressed(key):
-              break
-
-  audio_data = np.concatenate(audio_buffer, axis=0)
-
-  print("✅ Stopped Recording")
-
-  with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as fp:
-      write(fp.name, fs, audio_data)
-      return fp.name
-  
 def transcribe(audio_path):
-  result = model.transcribe(audio_path)
-  return result['text']
+    result = model.transcribe(audio_path)
+    return result['text']
